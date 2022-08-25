0a7a09df667ebcf23d21fb4780a70f2c4c44ff38
Ally <allygoose93@gmail.com>
2022-08-24 21:11:26 -0700

    created tag model

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from recipes.models import Recipe, Measure, FoodItem, Ingredient, Step
+from tags.models import Tag
 
 # Register your models here.
 admin.site.register(Recipe)
@@ -7,3 +8,5 @@ admin.site.register(Measure)
 admin.site.register(FoodItem)
 admin.site.register(Ingredient)
 admin.site.register(Step)
+
+admin.site.register(Tag)
--- tags/models.py
+++ tags/models.py
@@ -1,6 +1,10 @@
-# from django.db import models
+from django.db import models
 
 
 # Create your models here. have it quotes
-# class Tag(models.Model):
-#     pass
+class Tag(models.Model):
+    name = models.CharField(max_length=20)
+    recipes = models.ManyToManyField("recipes.Recipe", related_name="tags")
+
+    def __str__(self):
+        return self.name
