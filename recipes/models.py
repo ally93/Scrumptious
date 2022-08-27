661806bbfb24b25feb7102d074a5311cf9d26648
Ally <allygoose93@gmail.com>
2022-08-27 13:30:41 -0700

    completed the ingredient and steps models and registered it in admin.py

--- recipes/models.py
+++ recipes/models.py
@@ -20,3 +20,38 @@ class Measure(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class FoodItem(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Ingredient(models.Model):
+    amount = models.FloatField()
+    recipe = models.ForeignKey(
+        "Recipe", related_name="ingredients", on_delete=models.CASCADE
+    )
+    measure = models.ForeignKey(
+        "Measure", related_name="measures", on_delete=models.PROTECT
+    )
+    food = models.ForeignKey(
+        "FoodItem", related_name="foods", on_delete=models.PROTECT
+    )
+
+    def __str__(self):
+        return str(self.food) + " " + str(self.amount) + " " + str(self.measure)
+
+
+class Step(models.Model):
+    recipe = models.ForeignKey(
+        "Recipe", related_name="steps", on_delete=models.CASCADE
+    )
+    order = models.PositiveSmallIntegerField()
+    directions = models.CharField(max_length=300)
+
+    def __str__(self):
+        return str(self.recipe)
+        # + " " + str(self.order) + " " + self.directions
